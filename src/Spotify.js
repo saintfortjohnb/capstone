a823cae9b2b2454d7374c6c4c194be6719dafc19
John Saint-Fort <123652151+saintfortjohnb@users.noreply.github.com>
2023-10-24 23:07:24 -0400

    Update Spotify.js

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const authEndpoint = 'https://accounts.spotify.com/authorize?';
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = 'https://echo-ggr6.onrender.com';
+const redirectUri = 'http://localhost:3000';
 const scopes = ['streaming','user-read-email','user-read-private','user-library-read','user-library-modify','user-read-playback-state','user-modify-playback-state','user-read-currently-playing','playlist-read-private','app-remote-control','user-read-playback-position','user-top-read','user-read-recently-played'];
 
 export const loginEndoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
